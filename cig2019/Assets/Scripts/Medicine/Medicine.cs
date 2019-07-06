4bfbe1eee5fbe4227a510cc394e70bbc05254b01
Lin <ljhwuwu@qq.com>
2019-07-07 02:24:13 +0800

    读取药和形状的数据单例

---
+++ cig2019/Assets/Scripts/Medicine/Medicine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medicine : MonoBehaviour
+{
+    public static Medicine Instance;
+    public List<List<string>> Data;
+    public List<List<string>> Shape;
+
+    private void Awake()
+    {
+        Instance = this;
+        Data = LoadExcelData.instance.LoadData("Medicine.xlsx");
+        Shape = LoadExcelData.instance.LoadData("Shape.xlsx");
+    }
+}
