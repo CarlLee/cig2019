fd718a306a88cc2f194bafb659c5225767c267fa
Lin <ljhwuwu@qq.com>
2019-07-06 17:55:19 +0800

    主逻辑

---
+++ cig2019/Assets/Scripts/ClinicManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClinicManager : MonoBehaviour
+{
+    public int MoneyHave = 0;
+    public int Day = 0;
+    public List<int> Medicines;
+
+    public void Initiate()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
