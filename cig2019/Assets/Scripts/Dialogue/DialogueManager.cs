a56863f1fc4cdcba3da9a8481bcb8f9eff84e642
Lin <ljhwuwu@qq.com>
2019-07-06 15:57:51 +0800

    放入读表相关工具

---
+++ cig2019/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Excel;
+using System.Data;
+using OfficeOpenXml;
+
+public class DialogueManager : MonoBehaviour
+{
+    public List<List<string>> Data;
+    public DialogueReader Patient;
+    public DialogueReader Select1;
+    public DialogueReader Select2;
+    public int Now = 0;
+
+    public void ReadData(string fileName)
+    {
+        Data = LoadExcelData.instance.LoadData(fileName);
+        for(int i = 0; i < Data.Count; i++)
+        {
+            for(int j = 0; j < Data[i].Count; j++)
+            {
+                Debug.Log(Data[i][j]);
+            }
+        }
+    }
+
+    public void DialogueStart()
+    {
+
+    }
+
+    private void Start()
+    {
+        ReadData("1.xlsx");
+        DialogueStart();
+    }
+}
---
+++ cig2019/Assets/Scripts/Dialogue/DialogueReader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueReader : MonoBehaviour
+{
+    public Text Text;
+
+    public List<string> Dialogues;
+    public List<int> Relating;
+    public int now = 0;
+
+    private void Awake()
+    {
+        Show(Dialogues[now]);
+    }
+
+    public void Next()
+    {
+        if (Relating[now] >= 0)
+        {
+            Show(Dialogues[Relating[now]]);
+            now = Relating[now];
+        }
+
+    }
+
+    public void Show(string str)
+    {
+        Text.text = str;
+    }
+}
+
